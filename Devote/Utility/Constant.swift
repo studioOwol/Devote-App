3545c9a82e0649bcca06d9c95a475c565cb544c7
studioOwol <from.owol@gmail.com>
2023-04-25 16:00:27 +0900

    Core Data Introduction

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Nahyun on 2023/04/25.
+//
+
+import SwiftUI
+
+//MARK: - Formatter
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+//MARK: - UI
+
+//MARK: - UX

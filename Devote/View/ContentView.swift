614201edbc1794f1d94970f06a36cacbe0da5317
studioOwol <from.owol@gmail.com>
2023-04-25 17:37:49 +0900

    Custom Header

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -10,6 +10,7 @@ import CoreData
 
 struct ContentView: View {
     //MARK: - Property
+    @AppStorage("isDarkMode") private var isDarkMode: Bool = false
     @State var task: String = ""
     @State private var showNewTaskItem: Bool = false
     
@@ -45,6 +46,39 @@ struct ContentView: View {
                 //MARK: - Main View
                 VStack{
                     //MARK: - Header
+                    
+                    HStack(spacing:10) {
+                        // Title
+                        Text("Devote")
+                            .font(.system(.largeTitle, design: .rounded))
+                            .fontWeight(.heavy)
+                            .padding(.leading, 4)
+                        
+                        Spacer()
+                        
+                        // Edit Button
+                        EditButton()
+                            .font(.system(size: 16, weight: .semibold, design: .rounded))
+                            .padding(.horizontal, 10)
+                            .frame(minWidth: 70, minHeight: 24)
+                            .background(
+                                Capsule().stroke(Color.white, lineWidth: 2)
+                            )
+                        
+                        // Appearance Button (Light/Dark Mode)
+                        Button(action: {
+                            // Toggle Appearance
+                            isDarkMode.toggle()
+                        }, label: {
+                            Image(systemName: isDarkMode ? "moon.circle.fill" : "moon.circle")
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .font(.system(.title, design: .rounded))
+                        })
+                        
+                    }//: HStack
+                    .padding()
+                    .foregroundColor(.white)
                     
                     Spacer(minLength: 80)
                     
@@ -107,14 +141,8 @@ struct ContentView: View {
                 UITableView.appearance().backgroundColor = UIColor.clear
             }
             .navigationBarTitle("Daily Tasks", displayMode: .large)
+            .navigationBarHidden(true)
             
-            .toolbar {
-            #if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-            #endif
-            }//: Toolbar
             .background(
                 BackgroundImageView()
             )

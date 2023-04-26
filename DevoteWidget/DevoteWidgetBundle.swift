cc3277a30098e6e83c51960e59c01de2f6f07766
studioOwol <from.owol@gmail.com>
2023-04-26 12:33:37 +0900

    Widget API

---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by Nahyun on 2023/04/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+    }
+}
